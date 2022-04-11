4763dd927137618a549ac0d22ef67abbacbfedb7
sahinmeric <sahin.meric86@gmail.com>
2022-04-10 23:09:47 -0300

    access denied?

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,7 +2,7 @@
 and creates a new user with all privilages on all the tables of hbnb_dev_db
 and select privilages on all the tables of performance_schema*/
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED WITH authentication_plugin BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO hbnb_dev@localhost;
 GRANT SELECT ON performance_schema.* TO hbnb_dev@localhost;
 FLUSH PRIVILEGES;
