89f55eaf1b8b40fde51288f465b414c51a6310e5
sahinmeric <sahin.meric86@gmail.com>
2022-05-15 13:56:54 -0300

    start flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module that starts flask web app
+"""
+from email.policy import strict
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    """greeting"""
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
+
